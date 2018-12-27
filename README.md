e08572f9daabc44f5250b9961348e71a0132b024
Chen Hui Jing <huijing.chen@vonage.com>
2018-12-27 11:10:02 +0800

    Modify with own information

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-It's me, Tierney!
+It's an NPM card! 🤓
 
-# Usage
+Forked from [Tierney Cyren](https://github.com/bnb/bitandbang) 
+You can make your own too, instructions by [Conlin Durbin](https://dev.to/wuz/setting-up-a-npx-username-card-1pip)
+
+## Usage
 Via npx:
 ```
-npx bitandbang
+npx huijing
 ```
 
 Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
--- build.js
+++ build.js
@@ -15,33 +15,27 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
-  labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelnpm: chalk.white.bold('        npm:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:')
+  name: chalk.white('Chen Hui Jing'),
+  handle: chalk.white('hj_chen'),
+  work: chalk.white('Developer Advocate at Nexmo'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('hj_chen'),
+  github: chalk.gray('https://github.com/') + chalk.green('huijing'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('huijingchen'),
+  web: chalk.cyan('https://www.chenhuijing.com'),
+  npx: chalk.red('npx') + ' ' + chalk.white('huijing'),
+  labelWork: chalk.white.bold('Work:'),
+  labelTwitter: chalk.white.bold('Twitter:'),
+  labelGitHub: chalk.white.bold('GitHub:'),
+  labelLinkedIn: chalk.white.bold('LinkedIn:'),
+  labelWeb: chalk.white.bold('Web:'),
+  labelCard: chalk.white.bold('Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
-const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
-const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
@@ -51,9 +45,7 @@ const carding = `${data.labelCard}  ${data.npx}`
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
-               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
                twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
                webing + newline + newline + // data.labelWeb + data.web
